5f5cc333f1f25c16f48e8cf1f547b821c6c1b031
Võ Thanh Duy <hcmc.duyvo@gmail.com>
2026-06-05 18:44:20 +0700

    refactor: update and cleanup challenge scripts
    Update initialization in `get_sol3.ts` to use local config and remove deprecated utility scripts `query_challenge.ts` and `update_challenge.ts`.

--- get_sol3.ts
+++ get_sol3.ts
@@ -1,13 +1,19 @@
-import { db } from './src/firebase.ts';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import * as fs from 'fs';
+
+const firebaseConfig = JSON.parse(fs.readFileSync('./firebase-applet-config.json', 'utf8'));
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 
 async function run() {
   const querySnapshot = await getDocs(collection(db, 'challenges'));
   querySnapshot.forEach((doc) => {
     const data = doc.data();
     if (data.solution && data.solution.includes('Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff.')) {
+        console.log('ID:', data.id);
         console.log('Title:', data.title);
-        console.log('Solution:', data.solution);
+        // console.log('Solution:', data.solution);
         console.log('-------------');
     }
   });
